f6b1c4799bc7b5e868d72f2679becc4c31b4cc31
MEGA <MEGA@gmail.com>
2025-02-17 17:17:41 +0200

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static int Power(int a, int b)
+        {
+           
+            if (b == 0)
+                return 1;
+
+            
+            return a * Power(a, b - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Power(2, 3));
+        }
+    }
+}
